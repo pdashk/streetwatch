8c5973e51c0aaae67d412bd81579ae9b0e32536a
Derek Wen <derek.hz.wen@gmail.com>
2023-11-21 14:27:06 -0800

    Added derek.py and modified collect_images.py

--- scripts/collect_images.py
+++ scripts/collect_images.py
@@ -11,7 +11,9 @@ OUTPATH = 'data/images/'
 
 
 from collect_images.phi import create_images as images_phi
+from collect_images.derek import derek_create_images
 
 
 if __name__== "__main__":
     images_phi(INPUT, OUTPATH)
+    derek_create_images()
