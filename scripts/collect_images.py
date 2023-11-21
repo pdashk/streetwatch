d578aa1bd4c628e546978a5df438fc451a73bf45
Brandon Luong <brandonluong21@gmail.com>
2023-11-21 11:22:48 -0800

    added brandon.py and edited collect_images.py

--- scripts/collect_images.py
+++ scripts/collect_images.py
@@ -11,7 +11,8 @@ OUTPATH = 'data/images/'
 
 
 from collect_images.phi import create_images as images_phi
-
+from collect_images.brandon import collect_imgs as images_brandon
 
 if __name__== "__main__":
     images_phi(INPUT, OUTPATH)
+    images_brandon(OUTPATH)
