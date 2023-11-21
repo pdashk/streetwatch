83895613177f51f170678efd5947b1de9f653723
kellyhpark <kellypark100@gmail.com>
2023-11-21 14:53:29 -0800

    Added kelly.py and edited collect_images.py

--- scripts/collect_images.py
+++ scripts/collect_images.py
@@ -11,7 +11,8 @@ OUTPATH = 'data/images/'
 
 
 from collect_images.phi import create_images as images_phi
-
+from collect_images.kelly import collect_imgs as images_kelly
 
 if __name__== "__main__":
     images_phi(INPUT, OUTPATH)
+    images_kelly(OUTPATH)
